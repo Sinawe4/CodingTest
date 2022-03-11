7230643b420e860022929d13aa9ae85effb9c396
YongWoo <aba1430@naver.com>
2022-03-11 10:47:05 +0900

    [ADD] 소수 구하기 문제

---
+++ src/main/Test/PrimeNumber.java
@@ -0,0 +1,34 @@
+package main.Test;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+
+        Scanner sc =new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+
+        for(int i = 0; i <= n; i++){
+            if(PrimeNumber(i) ==true)
+                count++;
+        }
+
+        System.out.println(count);
+
+    }
+
+    public static boolean PrimeNumber (int number) {
+        if(number < 2) {
+            return false;
+        }
+
+        for(int i = 2; i * i <= number; i++) {
+            if(number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
