84ef899b50a68b02e95e85f83501be0b091e4e6e
YongWoo <aba1430@naver.com>
2022-03-21 10:46:42 +0900

    [ADD] Alignment level 1

---
+++ src/main/Test/programmers/Alignment/_1_K번째수.java
@@ -0,0 +1,22 @@
+package main.Test.programmers.Alignment;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class _1_K번째수 {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = {};
+        for (int i = 0; i < commands.length; i++){
+
+
+
+
+
+
+
+        }
+
+
+        return answer;
+    }
+}
