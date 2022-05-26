da9b5e634c8d8c9704556fa506655a7b17c24451
YongWoo <aba1430@naver.com>
2022-05-26 20:57:39 +0900

    덩치

---
+++ src/main/Test/solved/class2/BaekJoon_7568.java
@@ -0,0 +1,28 @@
+package main.Test.solved.class2;
+
+import java.util.Scanner;
+
+public class BaekJoon_7568 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        int[] height = new int[count];
+        int[] weight = new  int[count];
+        int[] rank = new int[count];
+        for (int i = 0; i < count; i++){
+            height[i] = sc.nextInt();
+            weight[i] = sc.nextInt();
+        }
+        for (int i = 0; i < count; i++){
+            rank[i] = 1;
+            for (int j = 0 ; j < count; j++){
+                if (i==j)
+                    continue;
+                if (height[i] < height[j] && weight[i] < weight[j])
+                    rank[i]++;
+            }
+            System.out.println(rank[i]);
+        }
+    }
+}
+
