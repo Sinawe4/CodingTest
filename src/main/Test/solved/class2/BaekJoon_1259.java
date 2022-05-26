910fc03956c517a4dc0d3b261fecc05fc8b698a7
YongWoo <aba1430@naver.com>
2022-05-26 17:46:19 +0900

    [ADD] Baekjoon

---
+++ src/main/Test/solved/class2/BaekJoon_1259.java
@@ -0,0 +1,37 @@
+package main.Test.solved.class2;
+
+import java.util.Scanner;
+
+public class BaekJoon_1259 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true){
+            String line = sc.next();
+            if (line.equals("0"))
+                System.exit(0);
+
+            String[] line1 = new String[line.length()];
+            String[] line2 = new String[line.length()];
+
+            for (int i = 0; i < line.length(); i++)
+                line1[i] = line.substring(i,i+1);
+
+
+            for (int i = 0; i < line.length(); i++)
+                line2[i] = line1[line.length()-1-i];
+            int count = 0;
+
+            for (int i = 0; i < line.length(); i++){
+                if (line1[i].equals(line2[i]))
+                    count++;
+            }
+            if (count == line.length())
+                System.out.println("yes");
+            else
+                System.out.println("no");
+
+        }
+    }
+}
+
