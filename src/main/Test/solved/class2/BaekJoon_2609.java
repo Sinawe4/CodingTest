6c54c64bbec726d122de2bfe02ed52cb0e343f8a
YongWoo <aba1430@naver.com>
2022-05-26 20:15:23 +0900

    최대공약수 최소공배수

---
+++ src/main/Test/solved/class2/BaekJoon_2609.java
@@ -0,0 +1,20 @@
+package main.Test.solved.class2;
+
+import java.util.Scanner;
+
+public class BaekJoon_2609 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int factor = gcd(a,b);
+        System.out.println(factor);
+        System.out.println(a*b/factor);
+    }
+    public static int gcd(int a, int b){
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
+    }
+}
+
